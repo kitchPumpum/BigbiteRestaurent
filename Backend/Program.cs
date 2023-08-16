d1cc8ac70a87598e5cf198a867b1ec204ddd8757
Deepak <deepakrattu.dr@gmail.com>
2023-08-16 15:24:57 -0400

    Backend Setup

---
+++ Backend/Program.cs
@@ -0,0 +1,32 @@
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BackendDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BackendConnectionString")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
