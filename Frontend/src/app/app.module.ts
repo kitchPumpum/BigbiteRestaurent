1ccf58bdd8e863e68448857f045b238c29a906f1
Anmol <anmolpreet.k067@gmail.com>
2023-08-21 15:18:33 -0400

    https://github.com/Anmolk067/cart.git

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
