2537d8561c4c883f22a462d714bd2aaee5122617
Deepak <deepakrattu.dr@gmail.com>
2023-08-22 13:29:54 -0400

    order-online component

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderOnlineComponent } from './order-online/order-online.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+    {  path: 'order-online', component: OrderOnlineComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
