81ba33496ae6f95e069857757025aa1b79954f65
hanufa <hanufazaaman@gmail.com>
2023-08-31 12:41:00 -0400

    worked with carousel to change the Top image automatically

--- Frontend/src/app/body/body.component.ts
+++ Frontend/src/app/body/body.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
 })
-export class BodyComponent {
+export class BodyComponent implements OnInit{
+  images: string[] = [
+    '../../assets/Images/BigBite.png',
+    '../../assets/Images/kabab.jpg',
+    '../../assets/Images/arabicfood.jpg',
+    '../../assets/Images/bbq.jpg',
+    '../../assets/Images/dinner.jpg',
+    '../../assets/Images/dessert.jpg',
+    '../../assets/Images/coldjuice.jpg'
+  ];
 
-}
+  currentIndex: number = 0;
+
+  ngOnInit() {
+    this.startCarousel();
+  }
+    startCarousel() {
+      setInterval(() => {
+        this.currentIndex = (this.currentIndex + 1) % this.images.length;
+      }, 5000); // Change image every 5 seconds
+    }
+  }
